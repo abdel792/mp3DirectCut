75e0e600d92dce5c3db244bf20daf8819fc38ae3
abdel792 <abdelkrim.bensaid@gmail.com>
2021-10-23 13:09:26 +0200

    Created the stable version 21.11 including compatibility for new syntaxes of the controlTypes module.

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""some commands to use mp3directcut with NVDA"""),
 	# version
-	"addon_version" : "21.11-dev",
+	"addon_version" : "21.11",
 	# Author(s)
 	"addon_author" : u"Abdel, Rèmy Ruiz, Abdellah Zineddine, Jean-François Colas",
 	# URL for the add-on documentation support

1440f805d7c2c6efc02b716a1ec720fe7541d435
abdel792 <abdelkrim.bensaid@gmail.com>
2023-05-07 15:45:25 +0200

    Created the version 23.05 including compatibility with nvda-2023.1 as well as L10N updates

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""some commands to use mp3directcut with NVDA"""),
 	# version
-	"addon_version" : "22.06",
+	"addon_version" : "23.05",
 	# Author(s)
 	"addon_author" : u"Abdel, Rèmy Ruiz, Abdellah Zineddine, Jean-François Colas",
 	# URL for the add-on documentation support
@@ -35,7 +35,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion" : "2016.4",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2022.1",
+	"addon_lastTestedNVDAVersion" : "2023.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!

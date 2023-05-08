13261d8a227cf2f2c83aa1ef3a869e1d3bd54e49
abdel792 <abdelkrim.bensaid@gmail.com>
2023-05-08 23:13:00 +0200

    Created stable version 20230508.0.0 including compliance with the new addon store system recommendations for version numbering

--- buildVars.py
+++ buildVars.py
@@ -16,33 +16,38 @@ def _(arg):
 # Add-on information variables
 addon_info = {
 	# add-on Name/identifier, internal for NVDA
-	"addon_name" : "mp3DirectCut",
+	"addon_name": "mp3DirectCut",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on
 	# to be shown on installation and add-on information found in Add-ons Manager.
-	"addon_summary" : "mp3DirectCut",
+	"addon_summary": "mp3DirectCut",
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description" : _("""some commands to use mp3directcut with NVDA"""),
+	"addon_description": _("""some commands to use mp3directcut with NVDA"""),
 	# version
-	"addon_version" : "23.05",
+	"addon_version": "20230508.0.0",
 	# Author(s)
-	"addon_author" : u"Abdel, Rèmy Ruiz, Abdellah Zineddine, Jean-François Colas",
+	"addon_author": "Abdel, Rèmy Ruiz, Abdellah Zineddine, Jean-François Colas",
 	# URL for the add-on documentation support
-	"addon_url" : "http://addons.nvda-project.org",
+	"addon_url": "https://addons.nvda-project.org/addons/mp3DirectCut.en.html",
+	# URL for the add-on repository where the source code can be found
+	"addon_sourceURL": "https://github.com/abdel792/mp3DirectCut",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion" : "2016.4",
+	"addon_minimumNVDAVersion": "2019.1.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2023.1",
+	"addon_lastTestedNVDAVersion": "2023.1.0",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
 	"addon_updateChannel": None,
+	# Add-on license such as GPL 2
+	"addon_license": "GPL v2",
+	# URL for the license document the ad-on is licensed under
+	"addon_licenseURL": "https://www.gnu.org/licenses/gpl-2.0.html",
 }
 
-import os.path
 # Define the python files that are the sources of your add-on.
 # You can either list every file (using ""/") as a path separator,
 # or use glob expressions.
@@ -51,6 +56,7 @@ import os.path
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
+import os
 pythonSources = [os.path.join("addon", "globalPlugins", "mp3DirectCut", "*.py"), os.path.join("addon", "appModules", "*.py")]
 
 # Files that contain strings for translation. Usually your python sources
